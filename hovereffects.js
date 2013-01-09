7054b6b47e74e0763d35b654d7d4cc44a4e7ae39
AlisaEx <eatliightning@gmail.com>
2013-01-09 12:09:12 -0700

    finished cosplay page

--- hovereffects.js
+++ hovereffects.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
-	$("li").mouseover(function () {
-        $("#switchImg").replaceWith('<img src="http://i48.tinypic.com/231h6g.jpg">');
-       });
+	$("#lilith").hover(function () {
+        $(".switchImg").replaceWith('<img src="http://i48.tinypic.com/231h6g.jpg" class="switchImg">');
+	})
+	$("#zombie").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/zombie_zps4b551beb.jpg" class="switchImg">');
+	})
+	$("#doctor").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/11dr_zps8a9bb71a.jpg" class="switchImg">');
+	})
+	$("#ramona").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/ramona_zps531a9a52.jpg" class="switchImg">');
+	})
+	$("#bmo").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/bmo_zps1f14e570.jpg" class="switchImg">');
+	})
+	$("#steam").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/steam_zps7f79dd19.jpg" class="switchImg">');
+	})
+	$("#death").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/deathkid_zps2775daad.jpg" class="switchImg">');
+	})
+	$("#keroro").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/keroro_zps3a1b80c6.jpg" class="switchImg">');
+	})
+	$("#robin").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i47.tinypic.com/2h6r3ww.jpg" class="switchImg">');
+	})
+	$("#shake").hover(function(){
+		$(".switchImg").replaceWith('<img src="http://i1276.photobucket.com/albums/y464/alisaex/297782_256883957774716_870032232_n_zpsb0592cce.jpg" class="switchImg">');
+	})
 });
